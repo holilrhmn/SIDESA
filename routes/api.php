66a4366731fe770e7affecb28bc93542b81e6cf7
holilrhmn <holilrahman10@gmail.com>
2020-06-17 09:39:07 +0800

    Perbaikan Fitur

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,21 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/perkebunan','Api\PerkebunanController@all');
+Route::get('/perkebunan/{id}','Api\PerkebunanController@show');
+Route::post('/perkebunan','Api\PerkebunanController@store');
+Route::put('/perkebunan/{id}','Api\PerkebunanController@update');
+Route::delete('/perkebunan/{id}','Api\PerkebunanController@delete');
+
+Route::get('/warga','Api\WargaController@all');
+Route::get('/warga/{id}','Api\WargaController@show');
+Route::post('/warga','Api\WargaController@store');
+Route::put('/warga/{id}','Api\WargaController@update');
+Route::delete('/warga/{id}','Api\WargaController@delete');
+
+Route::get('/pengumuman','Api\PengumumanController@all');
+Route::get('/pengumuman/{id}','Api\PengumumanController@show');
+Route::post('/pengumuman','Api\PengumumanController@store');
+Route::put('/pengumuman/{id}','Api\PengumumanController@update');
+Route::delete('/pengumuman/{id}','Api\PengumumanController@delete');
